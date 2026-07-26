2e4b20279df58a1c93aa221d6012084295bdaa98
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-07-26 13:33:50 +0200

    Initialise les passerelles locales réelles

---
+++ auralive/gateways/__init__.py
@@ -0,0 +1,11 @@
+from .mairaiy_http import MairaiyHttpGateway
+from .obs_websocket import ObsWebSocketGateway
+from .overlay import OverlayHub
+from .twitch_helix import TwitchHelixGateway
+
+__all__ = [
+    "MairaiyHttpGateway",
+    "ObsWebSocketGateway",
+    "OverlayHub",
+    "TwitchHelixGateway",
+]
