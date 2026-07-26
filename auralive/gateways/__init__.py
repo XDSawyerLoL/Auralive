9bd6afb5def4db5861d89d8f44c9b50ed9c556e9
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-07-26 13:43:35 +0200

    Expose la passerelle EventSub native

--- auralive/gateways/__init__.py
+++ auralive/gateways/__init__.py
@@ -1,11 +1,13 @@
 from .mairaiy_http import MairaiyHttpGateway
 from .obs_websocket import ObsWebSocketGateway
 from .overlay import OverlayHub
+from .twitch_eventsub import TwitchEventSubGateway
 from .twitch_helix import TwitchHelixGateway
 
 __all__ = [
     "MairaiyHttpGateway",
     "ObsWebSocketGateway",
     "OverlayHub",
+    "TwitchEventSubGateway",
     "TwitchHelixGateway",
 ]
