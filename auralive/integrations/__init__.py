e03a29dd5fd7826f35a0b07d049362047460008b
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-07-26 13:15:06 +0200

    Initialise le paquet d’intégrations natives

---
+++ auralive/integrations/__init__.py
@@ -0,0 +1,15 @@
+from .base import (
+    MairaiyGateway,
+    ObsGateway,
+    OverlayGateway,
+    ServiceUnavailable,
+    TwitchGateway,
+)
+
+__all__ = [
+    "MairaiyGateway",
+    "ObsGateway",
+    "OverlayGateway",
+    "ServiceUnavailable",
+    "TwitchGateway",
+]
